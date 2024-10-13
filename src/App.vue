44d138d64c6491bb0ece683c8892984662d65563
liz <liz.yanez.lobe@gmail.com>
2024-10-13 13:33:28 -0300

    cambios en app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,66 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/posts">Posts</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <header>
+      <h1>Mi Aplicación Vue</h1>
+      <nav>
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
+          <li><router-link to="/contact">Contact</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
+  margin-top: 20px;
+}
+
+header {
+  background-color: #42b983;
+  padding: 10px;
+  color: white;
 }
 
 nav {
-  padding: 30px;
+  margin: 10px 0;
 }
 
-nav a {
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline;
+  margin-right: 15px;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+a.router-link-exact-active {
   font-weight: bold;
-  color: #2c3e50;
+  text-decoration: underline;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+main {
+  margin-top: 20px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,27 +3,23 @@ import HomeView from '../views/HomeView.vue';
 import AboutView from '../views/AboutView.vue';
 import ContactView from '../views/ContactView.vue';
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: AboutView
-  },
-  {
-    path: '/contact',
-    name: 'Contact',
-    component: ContactView
-  }
-];
-
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
+    }
+  ] 
 });
-
-export default router;
